3fa53a39a98b0a371d3f22cb168b57c2f1f9bb6d
HunterWilkins <wilkins.hunter@gmail.com>
2021-04-10 09:52:45 -0500

    Initialized Repo with only Unsolved Folder

---
+++ HTML-Gen-Unsolved/index.js
@@ -0,0 +1,15 @@
+// To Do: Require inquirer and fs
+
+// To Do: Create a Questions Array compatible with Inquirer
+//  (to ask the user what elements they want on the page)
+//  Questions:  "What should the Header say?"
+//              "How many nav links should there be?"
+//              "How many lines of lorem ipsum text do you want?"
+//              "Would you like to include a picture of two lovely pigs?" (yes/no)
+//              "What should the Footer Say?"
+
+// To Do: Create a Function for Prompting the User with your questions and Inquirer.prompt
+
+// To Do: Create a Function for Generating the HTML based on the answers of the prompt
+
+// Call the function and run the program
